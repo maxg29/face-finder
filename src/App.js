3cb5697bb4c2fd5aadea7e0aa87eae903c8a3e63
Maxime Gagnon <maxime_gagnon@hotmail.com>
2018-08-10 18:13:53 -0400

    Initial version

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'cc26d6bf12e04bb285d07346e33c0a13'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 75,
+      density: {
+        enable: true,
+        value_area: 600
+      }
+    }
+  }
+}
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+          // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />        
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>      
+      <div className='white f3'>
+        {'Welcome to'}
+      </div>
+      <div className='white f1'>
+        {'FaceFinder!'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
